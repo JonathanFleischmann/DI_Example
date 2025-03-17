fb416815425ec72237cbe70a60a0914192e97e6b
JonathanFleischmann <i22006@hb.dhbw-stuttgart.degit config --global user.name>
2025-03-17 20:52:35 +0100

    refactor: update Client and Context classes for improved dependency injection and configuration

--- src/main/java/com/design_patterns/composite/dependency_injection/Client.java
+++ src/main/java/com/design_patterns/composite/dependency_injection/Client.java
@@ -2,28 +2,25 @@ package com.design_patterns.composite.dependency_injection;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.design_patterns.composite.dependency_injection")
+@EnableAutoConfiguration
+@ComponentScan
 public class Client {
-
-    private final OrderFactory orderFactory;
-
-    public Client(OrderFactory orderFactory) {
-        this.orderFactory = orderFactory;
-    }
-
-    @Bean
-    public ApplicationRunner applicationRunner() {
-        return args -> {
-            System.out.println(this.orderFactory.order().getDescription());
-            System.out.println(this.orderFactory.order().getPriceCalculation());
-            System.out.println("Price: " + this.orderFactory.order().getPrice());
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Client.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(OrderFactory orderFactory) {
+        return args -> {
+            Article article = orderFactory.order();
+            System.out.println(article.getDescription());
+            System.out.println(article.getPriceCalculation());
+            System.out.println("Price: " + article.getPrice());
+        };
     }
 }
